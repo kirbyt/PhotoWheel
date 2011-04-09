601d2133f46baa155dd14c9d51afe27417604fcd
Kirby Turner <kirby@thecave.com>
2011-04-09 18:24:44 -0400

    Wired the touch gesture.

--- PhotoWheel/Classes/ViewControllers/PhotoWheelImageViewController.m
+++ PhotoWheel/Classes/ViewControllers/PhotoWheelImageViewController.m
@@ -27,5 +27,42 @@
    [newView release];
 }
 
+- (void)viewDidLoad
+{
+   [super viewDidLoad];
+   
+   UITapGestureRecognizer *doubleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTapped:)];
+   [doubleTapGesture setNumberOfTapsRequired:2];
+   [[self view] addGestureRecognizer:doubleTapGesture];
+   [doubleTapGesture release];
+   
+   UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped:)];
+   [tapGesture requireGestureRecognizerToFail:doubleTapGesture];
+   [[self view] addGestureRecognizer:tapGesture];
+   [tapGesture release];
+   
+   UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinch:)];
+   [[self view] addGestureRecognizer:pinchGesture];
+   [pinchGesture release];
+}
+
+
+#pragma mark -
+#pragma mark UIGestureRecognizer Handlers
+
+- (void)tapped:(UITapGestureRecognizer *)recognizer
+{
+   NSLog(@"tap");
+}
+
+- (void)doubleTapped:(UITapGestureRecognizer *)recognizer
+{
+   NSLog(@"tap tap");
+}
+
+- (void)pinch:(UIPinchGestureRecognizer *)recognizer
+{
+   NSLog(@"pinch/zoom");
+}
 
 @end
