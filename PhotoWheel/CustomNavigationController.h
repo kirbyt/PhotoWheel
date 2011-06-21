b15db085dbb7d585f4bb03f34d442dec784b1469
Kirby Turner <kirby@thecave.com>
2011-06-21 07:52:21 -0400

    Added custom pop transition.

---
+++ PhotoWheel/CustomNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CustomNavigationController.h
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomNavigationController : UINavigationController
+
+@end
---
+++ PhotoWheel/CustomNavigationController.m
@@ -0,0 +1,71 @@
+//
+//  CustomNavigationController.m
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import "CustomNavigationController.h"
+#import "UIView+PWCategory.h"
+#import "PhotoBrowserViewController.h"
+
+@implementation CustomNavigationController
+
+- (UIViewController *)popViewControllerAnimated:(BOOL)animated
+{
+   // Customize the transition for popping from the 
+   // PhotoBrowserViewController only. If self is not
+   // the photo browser then fall back to the default
+   // transition.
+   UIViewController *sourceViewController = [self topViewController];
+   if ([sourceViewController isKindOfClass:[PhotoBrowserViewController class]] == NO) {
+      return [super popViewControllerAnimated:animated];
+   }
+   
+   // Animates image snapshot of the view.
+   UIView *sourceView = [sourceViewController view];
+   UIImage *sourceViewImage = [sourceView pw_imageSnapshot];
+   UIImageView *sourceImageView = [[UIImageView alloc] initWithImage:sourceViewImage];
+   
+   NSArray *viewControllers = [self viewControllers];
+   NSInteger count = [viewControllers count];
+   NSInteger index = count - 2;
+   
+   UIViewController *destinationViewController =[viewControllers objectAtIndex:index];
+   UIView *destinationView = [destinationViewController view];
+   
+   [super popViewControllerAnimated:NO];
+   [destinationView addSubview:sourceImageView];
+   
+   CGRect frame = [destinationView frame];
+   CGPoint shrinkToPoint = CGPointMake(frame.size.width / 2, frame.size.height / 2);
+   
+   void (^animations)(void) = ^ {
+      [sourceImageView setFrame:CGRectMake(shrinkToPoint.x, shrinkToPoint.y, 0, 0)];
+      [sourceImageView setAlpha:0.0];
+      
+      // Animate the nav bar too.
+      UINavigationBar *navBar = [self navigationBar];
+      [navBar setFrame:CGRectOffset(navBar.frame, 0, -navBar.frame.size.height)];
+   };
+   
+   void (^completion)(BOOL) = ^(BOOL finished) {
+      [self setNavigationBarHidden:YES];
+      // Reset the nav bar position.
+      UINavigationBar *navBar = [self navigationBar];
+      [navBar setFrame:CGRectOffset(navBar.frame, 0, navBar.frame.size.height)];
+      
+      [sourceImageView removeFromSuperview];
+   };
+   
+   [UIView transitionWithView:destinationView
+                     duration:0.3
+                      options:UIViewAnimationOptionTransitionNone
+                   animations:animations
+                   completion:completion];
+   
+   return sourceViewController;
+}
+
+@end
