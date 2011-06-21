da1a8bf4cebd7609b5fbcb6a33f9866c7e0cae4d
Kirby Turner <kirby@thecave.com>
2011-06-21 07:41:27 -0400

    Added custom push segue.

---
+++ PhotoWheel/CustomPushSegue.h
@@ -0,0 +1,13 @@
+//
+//  CustomPushSegue.h
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomPushSegue : UIStoryboardSegue
+
+@end
---
+++ PhotoWheel/UIView+PWCategory.h
@@ -0,0 +1,15 @@
+//
+//  UIView+PWCategory.h
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UIView_PWCategory)
+
+- (UIImage *)pw_imageSnapshot;
+
+@end
