9d9256846bf37ac7cca2b2655f6986f3eb3ef29f
Kirby Turner <kirby@thecave.com>
2011-06-21 07:13:19 -0400

    Added about view controller. Implement Done button on the about scene.

---
+++ PhotoWheel/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ PhotoWheel/AboutViewController.m
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.m
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/21/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+
+- (IBAction)done:(id)sender {
+   [self dismissModalViewControllerAnimated:YES];
+}
+
+@end
