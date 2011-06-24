b64b48acb2b3c42bbf273510cfbe1b4139acbe59
Kirby Turner <kirby@thecave.com>
2011-06-24 15:51:53 -0400

    Added Chapter 8 source code.

---
+++ PhotoWheelPrototype/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  PhotoWheelPrototype
+//
+//  Created by Kirby Turner on 6/15/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
