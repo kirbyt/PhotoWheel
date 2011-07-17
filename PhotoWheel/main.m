6b9aa21e3aca6b1720080f2d0f9b2516d61317fd
Kirby Turner <kirby@thecave.com>
2011-07-17 10:37:01 -0400

    Reset Chapter 14 source code.

---
+++ PhotoWheel/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 6/20/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PhotoWheelAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+   int retVal = 0;
+   @autoreleasepool {
+       retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([PhotoWheelAppDelegate class]));
+   }
+   return retVal;
+}
