302114395a8c67ef5c8aaaa832ec12c5f7027d5a
Kirby Turner <kirby@thecave.com>
2011-09-01 11:22:28 -0400

    Replaced old code with chapter 18 code. This is the new starting point for Chapter 19.

---
+++ PhotoWheel/PhotoWheel/ImageGridViewCell.m
@@ -0,0 +1,69 @@
+//
+//  ImageGridViewCell.m
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 8/22/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import "ImageGridViewCell.h"
+
+@interface ImageGridViewCell ()
+@property (nonatomic, strong, readwrite) UIImageView *imageView;
+@property (nonatomic, strong, readwrite) UIImageView *selectedIndicator;
+@end
+
+@implementation ImageGridViewCell
+
+@synthesize selected = selected_;
+@synthesize imageView = imageView_;
+@synthesize selectedIndicator = selectedIndicator_;
+
+- (void)commonInitWithSize:(CGSize)size
+{
+   CGRect frame = CGRectMake(0, 0, size.width, size.height);
+   [self setBackgroundColor:[UIColor clearColor]];
+   
+   self.imageView = [[UIImageView alloc] initWithFrame:frame];
+   [self addSubview:[self imageView]];
+   
+   NSInteger baseSize = 29;
+   self.selectedIndicator = [[UIImageView alloc] initWithFrame:CGRectMake(size.width - baseSize - 4, size.height - baseSize - 4, baseSize, baseSize)];
+   [[self selectedIndicator] setHidden:YES];
+
+   [self addSubview:[self selectedIndicator]];
+}
+
+- (id)init
+{
+   CGSize size = CGSizeMake(100, 100);
+   self = [self initWithSize:size];
+   if (self) {
+      
+   }
+   return self;
+}
+
+- (id)initWithSize:(CGSize)size
+{
+   CGRect frame = CGRectMake(0, 0, size.width, size.height);
+   self = [super initWithFrame:frame];
+   if (self) {
+      [self commonInitWithSize:size];
+   }
+   return self;
+}
+
+- (void)setSelected:(BOOL)selected
+{
+   selected_ = selected;
+   [[self selectedIndicator] setHidden:!selected];
+}
+
++ (ImageGridViewCell *)imageGridViewCellWithSize:(CGSize)size
+{
+   ImageGridViewCell *newCell = [[ImageGridViewCell alloc] initWithSize:size];
+   return newCell;
+}
+
+@end
---
+++ PhotoWheel/PhotoWheel/PhotoAlbumViewController.h
@@ -0,0 +1,31 @@
+//
+//  PhotoAlbumViewController.h
+//  PhotoWheel
+//
+//  Created by Kirby Turner on 8/13/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GridView.h"
+#import "PhotoBrowserViewController.h"
+
+@interface PhotoAlbumViewController : UIViewController <UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, NSFetchedResultsControllerDelegate, GridViewDataSource, PhotoBrowserViewControllerDelegate>
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong) NSManagedObjectID *objectID;
+@property (nonatomic, strong) IBOutlet UIToolbar *toolbar;
+@property (nonatomic, strong) IBOutlet UITextField *textField;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *addButton;
+@property (strong, nonatomic) IBOutlet GridView *gridView;
+@property (nonatomic, strong) IBOutlet UIImageView *backgroundImageView;
+@property (nonatomic, strong) IBOutlet UIImageView *shadowImageView;
+
+- (void)reload;
+- (IBAction)showActionMenu:(id)sender;
+- (IBAction)addPhoto:(id)sender;
+
+- (UIImage *)selectedImage;
+- (CGRect)selectedCellFrame;
+
+@end
