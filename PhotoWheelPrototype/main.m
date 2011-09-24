301132dcf86bf56bf4757a1bfcc871b81a6e8883
Kirby Turner <kirby@thecave.com>
2011-09-24 13:15:01 -0400

    Clean start with chapter 9 source code.

---
+++ PhotoWheelPrototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoWheelPrototype
+//
+//  Created by Kirby Turner on 9/24/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
