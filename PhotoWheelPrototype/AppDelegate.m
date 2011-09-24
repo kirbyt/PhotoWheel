32013bad458b68671df54b35115ba550877c8794
Kirby Turner <kirby@thecave.com>
2011-09-24 10:42:45 -0400

    Formatted code for book print.

--- PhotoWheelPrototype/AppDelegate.m
+++ PhotoWheelPrototype/AppDelegate.m
@@ -17,23 +17,35 @@
 @synthesize window = _window;
 @synthesize splitViewController = _splitViewController;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application 
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-
-   MasterViewController *masterViewController = [[MasterViewController alloc] initWithNibName:@"MasterViewController" bundle:nil];
-   UINavigationController *masterNavigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
-
-   DetailViewController *detailViewController = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
-   UINavigationController *detailNavigationController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
-
+   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+   // Override point for customization after application launch.
+   
+   MasterViewController *masterViewController = 
+      [[MasterViewController alloc] initWithNibName:@"MasterViewController" 
+                                             bundle:nil];
+   UINavigationController *masterNavigationController = 
+      [[UINavigationController alloc] 
+       initWithRootViewController:masterViewController];
+   
+   DetailViewController *detailViewController = 
+      [[DetailViewController alloc] initWithNibName:@"DetailViewController" 
+                                             bundle:nil];
+   UINavigationController *detailNavigationController = 
+      [[UINavigationController alloc] 
+       initWithRootViewController:detailViewController];
+   
    self.splitViewController = [[UISplitViewController alloc] init];
    self.splitViewController.delegate = detailViewController;
-   self.splitViewController.viewControllers = [NSArray arrayWithObjects:masterNavigationController, detailNavigationController, nil];
+   self.splitViewController.viewControllers = [NSArray arrayWithObjects:
+                                               masterNavigationController, 
+                                               detailNavigationController, 
+                                               nil];
    self.window.rootViewController = self.splitViewController;
-    [self.window makeKeyAndVisible];
-    return YES;
+   [self.window makeKeyAndVisible];
+   return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
--- PhotoWheelPrototype/DetailViewController.m
+++ PhotoWheelPrototype/DetailViewController.m
@@ -23,57 +23,57 @@
 
 - (void)setDetailItem:(id)newDetailItem
 {
-    if (_detailItem != newDetailItem) {
-        _detailItem = newDetailItem;
-        
-        // Update the view.
-        [self configureView];
-    }
-
-    if (self.masterPopoverController != nil) {
-        [self.masterPopoverController dismissPopoverAnimated:YES];
-    }        
+   if (_detailItem != newDetailItem) {
+      _detailItem = newDetailItem;
+      
+      // Update the view.
+      [self configureView];
+   }
+   
+   if (self.masterPopoverController != nil) {
+      [self.masterPopoverController dismissPopoverAnimated:YES];
+   }        
 }
 
 - (void)configureView
 {
-    // Update the user interface for the detail item.
-
+   // Update the user interface for the detail item.
+   
    if (self.detailItem) {
-       self.detailDescriptionLabel.text = [self.detailItem description];
+      self.detailDescriptionLabel.text = [self.detailItem description];
    }
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+   [super didReceiveMemoryWarning];
+   // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+   [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
    [self configureView];
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+   [super viewDidUnload];
+   // Release any retained subviews of the main view.
+   // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
+   [super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
+   [super viewDidAppear:animated];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -88,33 +88,34 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
+   // Return YES for supported orientations
    return YES;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+   if (self) {
       self.title = NSLocalizedString(@"Detail", @"Detail");
-    }
-    return self;
+   }
+   return self;
 }
-							
+
 #pragma mark - Split view
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
 {
-    barButtonItem.title = NSLocalizedString(@"Master", @"Master");
-    [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
-    self.masterPopoverController = popoverController;
+   barButtonItem.title = NSLocalizedString(@"Master", @"Master");
+   [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
+   self.masterPopoverController = popoverController;
 }
 
 - (void)splitViewController:(UISplitViewController *)splitController willShowViewController:(UIViewController *)viewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
 {
-    // Called when the view is shown again in the split view, invalidating the button and popover controller.
-    [self.navigationItem setLeftBarButtonItem:nil animated:YES];
-    self.masterPopoverController = nil;
+   // Called when the view is shown again in the split view, invalidating 
+   // the button and popover controller.
+   [self.navigationItem setLeftBarButtonItem:nil animated:YES];
+   self.masterPopoverController = nil;
 }
 
 @end
