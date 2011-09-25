d1528e77b5cf03a0e16b70570b7f849974291966
Kirby Turner <kirby@thecave.com>
2011-09-24 21:51:09 -0400

    Removed old project and replaced it with source code from Chapter 12. Fresh new start.

---
+++ PhotoWheelPrototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoWheelPrototype
+//
+//  Created by Kirby Turner on 9/24/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
--- PhotoWheelPrototype/DetailViewController.h
+++ PhotoWheelPrototype/DetailViewController.h
@@ -2,27 +2,21 @@
 //  DetailViewController.h
 //  PhotoWheelPrototype
 //
-//  Created by Kirby Turner on 6/15/11.
-//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//  Created by Kirby Turner on 9/24/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "WheelView.h"
-#import "PhotoAlbum.h"
-#import "Photo.h"
 
-@interface DetailViewController : UIViewController <UISplitViewControllerDelegate, WheelViewDataSource, UIActionSheetDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@interface DetailViewController : UIViewController 
+<UISplitViewControllerDelegate, WheelViewDataSource, UIActionSheetDelegate, 
+UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (strong, nonatomic) id detailItem;
 
 @property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 
-@property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
-
 @property (strong, nonatomic) IBOutlet WheelView *wheelView;
-
-@property (strong, nonatomic) PhotoAlbum *photoAlbum;
-
-- (IBAction)segmentedControlValueChanged:(id)sender;
 
 @end
