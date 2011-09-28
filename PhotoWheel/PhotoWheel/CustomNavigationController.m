030499b0a2cedec95a58c6a7109ad17359fa6dec
Kirby Turner <kirby@thecave.com>
2011-09-28 01:39:09 -0400

    Formated for print.

--- PhotoWheel/PhotoWheel/CustomNavigationController.m
+++ PhotoWheel/PhotoWheel/CustomNavigationController.m
@@ -15,20 +15,22 @@
 {
    UIViewController *sourceViewController = [self topViewController];
    
-   // Animates image snapshot of the view.
+   // Animates image snapshot of the view
    UIView *sourceView = [sourceViewController view];
    UIImage *sourceViewImage = [sourceView pw_imageSnapshot];
-   UIImageView *sourceImageView = [[UIImageView alloc] initWithImage:sourceViewImage];
+   UIImageView *sourceImageView = [[UIImageView alloc] 
+                                   initWithImage:sourceViewImage];
    
    NSArray *viewControllers = [self viewControllers];
    NSInteger count = [viewControllers count];
    NSInteger index = count - 2;
    
-   UIViewController *destinationViewController =[viewControllers objectAtIndex:index];
+   UIViewController *destinationViewController;
+   destinationViewController = [viewControllers objectAtIndex:index];
    UIView *destinationView = [destinationViewController view];
    UIImage *destinationViewImage = [destinationView pw_imageSnapshot];
-   UIImageView *destinationImageView = [[UIImageView alloc] initWithImage:destinationViewImage];
-   [destinationImageView setFrame:CGRectOffset([destinationImageView frame], 0, -[self navigationBar].frame.size.height)];
+   UIImageView *destinationImageView = [[UIImageView alloc] 
+                                        initWithImage:destinationViewImage];
    
    [super popViewControllerAnimated:NO];
    
@@ -36,20 +38,24 @@
    [destinationView addSubview:sourceImageView];
    
    CGRect frame = [destinationView frame];
-   CGPoint shrinkToPoint = CGPointMake(frame.size.width / 2, frame.size.height / 2);
+   CGPoint shrinkToPoint = CGPointMake(frame.size.width / 2, 
+                                       frame.size.height / 2);
    
    void (^animations)(void) = ^ {
-      [sourceImageView setFrame:CGRectMake(shrinkToPoint.x, shrinkToPoint.y, 0, 0)];
+      [sourceImageView setFrame:CGRectMake(shrinkToPoint.x, 
+                                           shrinkToPoint.y, 
+                                           0, 
+                                           0)];
       [sourceImageView setAlpha:0.0];
       
-      // Animate the nav bar too.
+      // Animate the nav bar too
       UINavigationBar *navBar = [self navigationBar];
       [navBar setFrame:CGRectOffset(navBar.frame, 0, -navBar.frame.size.height)];
    };
    
    void (^completion)(BOOL) = ^(BOOL finished) {
       [self setNavigationBarHidden:YES];
-      // Reset the nav bar position.
+      // Reset the nav bar position
       UINavigationBar *navBar = [self navigationBar];
       [navBar setFrame:CGRectOffset(navBar.frame, 0, navBar.frame.size.height)];
       
--- PhotoWheel/PhotoWheel/MainViewController.m
+++ PhotoWheel/PhotoWheel/MainViewController.m
@@ -6,25 +6,29 @@
 //  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
 //
 
-#import "MainViewController.h"
+#import "MainViewController.h"                                          // 1
 #import "PhotoAlbumViewController.h"
 #import "PhotoAlbumsViewController.h"
 
 @implementation MainViewController
 
-- (void)viewDidLoad
+- (void)viewDidLoad                                                     // 2
 {
    [super viewDidLoad];
    
-   UIStoryboard *storyboard = [self storyboard];
+   UIStoryboard *storyboard = [self storyboard];                        // 3
    
-   PhotoAlbumsViewController *photoAlbumsScene = [storyboard instantiateViewControllerWithIdentifier:@"PhotoAlbumsScene"];
-   [self addChildViewController:photoAlbumsScene];
-   [photoAlbumsScene didMoveToParentViewController:self];
-
-   PhotoAlbumViewController *photoAlbumScene = [storyboard instantiateViewControllerWithIdentifier:@"PhotoAlbumScene"];
-   [self addChildViewController:photoAlbumScene];
-   [photoAlbumScene didMoveToParentViewController:self];
+   PhotoAlbumsViewController *photoAlbumsScene;                         // 4
+   photoAlbumsScene = 
+      [storyboard instantiateViewControllerWithIdentifier:@"PhotoAlbumsScene"];           
+   [self addChildViewController:photoAlbumsScene];                      // 5
+   [photoAlbumsScene didMoveToParentViewController:self];               // 6
+   
+   PhotoAlbumViewController *photoAlbumScene;                           // 7
+   photoAlbumScene = [storyboard 
+                      instantiateViewControllerWithIdentifier:@"PhotoAlbumScene"];
+   [self addChildViewController:photoAlbumScene];                       // 8
+   [photoAlbumScene didMoveToParentViewController:self];                // 9
 }
 
 @end
