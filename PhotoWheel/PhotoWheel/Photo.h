8c1d34fdd716d94096d541c6102252e1f8a2b541
Kirby Turner <kirby@thecave.com>
2011-09-29 11:10:18 -0400

    Updated with the latest prototype code.

--- PhotoWheel/PhotoWheel/Photo.h
+++ PhotoWheel/PhotoWheel/Photo.h
@@ -2,8 +2,8 @@
 //  Photo.h
 //  PhotoWheelPrototype
 //
-//  Created by Tom Harrington on 6/30/11.
-//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//  Created by Kirby Turner on 9/24/11.
+//  Copyright (c) 2011 White Peak Software Inc. All rights reserved.
 //
 
 #import "_Photo.h"
