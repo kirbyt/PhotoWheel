998d984333eb3571de39ff8725f2645af61240f0
Kirby Turner <kirby@thecave.com>
2012-03-27 23:09:10 -0400

    Fixed typo.

--- PhotoWheel/PhotoWheel/AppKeys.h
+++ PhotoWheel/PhotoWheel/AppKeys.h
@@ -7,13 +7,13 @@
 //
 
 
-#error Changes the follow values to your own application key.
+#error Change the follow values to your own application key.
 
-// Changes this value to your own application key. More info at:
+// Change this value to your own application key. More info at:
 // http://www.flickr.com/services/api/misc.api_keys.html. 
 #define flickrAPIKey @"YOUR_FLICKR_APP_KEY"
 
 
-// Changes this value to your own application key. More info at:
+// Change this value to your own application key. More info at:
 // http://support.hockeyapp.net/kb/client-integration/crash-reporting-on-ios-quincykit
 #define QUINCYKIT_APPKEY @"YOUR_QUINCY_APP_KEY"
