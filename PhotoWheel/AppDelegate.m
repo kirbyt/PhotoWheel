bcc4a5fd8625c6b0723534812d60cc8a97b337bc
Kirby Turner <kirby@thecave.com>
2013-05-13 07:55:08 -0400

    Add migration map and migration code.

--- PhotoWheel/AppDelegate.m
+++ PhotoWheel/AppDelegate.m
@@ -146,8 +146,17 @@
    
    [self setPersistentStoreReady:NO];
    [SVProgressHUD showWithStatus:@"Syncing with iCloud" maskType:SVProgressHUDMaskTypeClear];
-   _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+
+   NSError *error = nil;
    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"PhotoWheel.sqlite"];
+   NSManagedObjectModel *model = [self managedObjectModel];
+   BOOL success = [self progressivelyMigrateURL:storeURL ofType:NSSQLiteStoreType toModel:model error:&error];
+   if (!success) {
+      // TODO: Alert the user.
+      DLog(@"Migration error: %@", [error localizedDescription]);
+   }
+
+   _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
    
    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
       // Build a URL to use as NSPersistentStoreUbiquitousContentURLKey
@@ -189,6 +198,99 @@
    });
    
    return _persistentStoreCoordinator;
+}
+
+#pragma mark - Progressive Migration -
+
+- (BOOL)progressivelyMigrateURL:(NSURL*)sourceStoreURL ofType:(NSString*)type toModel:(NSManagedObjectModel*)finalModel error:(NSError**)error
+{
+   NSDictionary *sourceMetadata = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:type URL:sourceStoreURL error:error];
+   if (!sourceMetadata) {
+      return NO;
+   }
+   
+   if ([finalModel isConfiguration:nil compatibleWithStoreMetadata:sourceMetadata]) {
+      *error = nil;
+      return YES;
+   }
+   
+   //Find the source model
+   NSManagedObjectModel *sourceModel = [NSManagedObjectModel mergedModelFromBundles:nil forStoreMetadata:sourceMetadata];
+   ZAssert(sourceModel != nil, @"Failed to find source model\n%@", sourceMetadata);
+
+   //Find all of the mom and momd files in the Resources directory
+   NSMutableArray *modelPaths = [NSMutableArray array];
+   NSArray *momdArray = [[NSBundle mainBundle] pathsForResourcesOfType:@"momd" inDirectory:nil];
+   for (NSString *momdPath in momdArray) {
+      NSString *resourceSubpath = [momdPath lastPathComponent];
+      NSArray *array = [[NSBundle mainBundle] pathsForResourcesOfType:@"mom" inDirectory:resourceSubpath];
+      [modelPaths addObjectsFromArray:array];
+   }
+   NSArray* otherModels = [[NSBundle mainBundle] pathsForResourcesOfType:@"mom" inDirectory:nil];
+   [modelPaths addObjectsFromArray:otherModels];
+   if (!modelPaths || ![modelPaths count]) {
+      //Throw an error if there are no models
+      NSDictionary *dict = @{NSLocalizedDescriptionKey:@"No models found in bundle."};
+      *error = [NSError errorWithDomain:@"PhotoWheel" code:8001 userInfo:dict];
+      return NO;
+   }
+   
+   //See if we can find a matching destination model
+   NSMappingModel *mappingModel = nil;
+   NSManagedObjectModel *targetModel = nil;
+   NSString *modelPath = nil;
+   for (modelPath in modelPaths) {
+      targetModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:[NSURL fileURLWithPath:modelPath]];
+      mappingModel = [NSMappingModel mappingModelFromBundles:nil forSourceModel:sourceModel destinationModel:targetModel];
+      //If we found a mapping model then proceed
+      if (mappingModel) {
+         break;
+      }
+   }
+   //We have tested every model, if nil here we failed
+   if (!mappingModel) {
+      NSDictionary *dict = @{NSLocalizedDescriptionKey:@"No models found in bundle."};
+      *error = [NSError errorWithDomain:@"PhotoWheel" code:8001 userInfo:dict];
+      return NO;
+   }
+   //We have a mapping model and a destination model.  Time to migrate
+   NSMigrationManager *manager = [[NSMigrationManager alloc] initWithSourceModel:sourceModel destinationModel:targetModel];
+   NSString *modelName = [[modelPath lastPathComponent] stringByDeletingPathExtension];
+   NSString *storeExtension = [[sourceStoreURL path] pathExtension];
+   NSString *storePath = [[sourceStoreURL path] stringByDeletingPathExtension];
+   //Build a path to write the new store
+   storePath = [NSString stringWithFormat:@"%@.%@.%@", storePath, modelName, storeExtension];
+   NSURL *destinationStoreURL = [NSURL fileURLWithPath:storePath];
+   if (![manager migrateStoreFromURL:sourceStoreURL
+                                type:type
+                             options:nil
+                    withMappingModel:mappingModel
+                    toDestinationURL:destinationStoreURL
+                     destinationType:type
+                  destinationOptions:nil
+                               error:error]) {
+      return NO;
+   }
+   //Migration was successful, move the files around to preserve the source
+   NSString *guid = [[NSProcessInfo processInfo] globallyUniqueString];
+   guid = [guid stringByAppendingPathExtension:modelName];
+   guid = [guid stringByAppendingPathExtension:storeExtension];
+   NSString *appSupportPath = [storePath stringByDeletingLastPathComponent];
+   NSString *backupPath = [appSupportPath stringByAppendingPathComponent:guid];
+   
+   NSFileManager *fileManager = [NSFileManager defaultManager];
+   if (![fileManager moveItemAtPath:[sourceStoreURL path] toPath:backupPath error:error]) {
+      //Failed to copy the file
+      return NO;
+   }
+   //Move the destination to the source path
+   if (![fileManager moveItemAtPath:storePath toPath:[sourceStoreURL path] error:error]) {
+      //Try to back out the source move first, no point in checking it for errors
+      [fileManager moveItemAtPath:backupPath toPath:[sourceStoreURL path] error:nil];
+      return NO;
+   }
+   //We may not be at the "current" model yet, so recurse
+   return [self progressivelyMigrateURL:sourceStoreURL ofType:type toModel:finalModel error:error];
 }
 
 #pragma mark - Report iCloud Error 
